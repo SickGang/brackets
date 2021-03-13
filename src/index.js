ac78d9832481824652c887762be6c06bde22d13b
Maksim <max.ru6@mail.ru>
2021-03-13 19:22:10 +0400

    feat: add new func check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let lengthOfStr = str.length;
+
+    while (lengthOfStr) {
+        for (let item of bracketsConfig) {
+            if (str.includes(item.join(""))) {
+                str = str.replace(item.join(""), "");
+            }
+        }
+        lengthOfStr--;
+    }
+    return !str.length;
+};
